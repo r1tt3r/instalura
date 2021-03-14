a55132f0c4516582f25dd8f3a859489724ac1c57
Leandro Ritter <leandroritter@MacBook-Pro-de-Leandro.local>
2021-03-14 10:03:02 -0300

    refactor(app.js): remove unsed components to eslint

--- pages/_app.js
+++ pages/_app.js
@@ -2,9 +2,6 @@
 import Head from 'next/head';
 import React from 'react';
 import PropTypes from 'prop-types';
-import { ThemeProvider } from 'styled-components';
-import theme from '../src/theme';
-import GlobalStyle from '../src/theme/GlobalStyles';
 
 export default function App({ Component, pageProps }) {
   return (
