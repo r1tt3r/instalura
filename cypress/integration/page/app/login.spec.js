4db25c0f89909e5163ac11f556d02cadea39894b
Leandro Ritter <leandroritter@MacBook-Pro-de-Leandro.local>
2021-03-26 12:20:08 -0300

    test(standardization of tests): test ajustments to be clear to read, add StagingEnv check

--- cypress/integration/page/app/login.spec.js
+++ cypress/integration/page/app/login.spec.js
@@ -1,26 +1,28 @@
 /// <reference types="cypress" />
 
+import LoginScreenPageObject from '../../../../src/components/screens/app/LoginScreen/LoginScreen.pageObject';
+
 describe('/pages/app/login', () => {
-  it('preencha os campos e va para pagina /app/profile', () => {
-    cy.intercept(
-      'https://instalura-api-git-master-omariosouto.vercel.app/api/login'
-    ).as('userLogin');
+  describe('when fill and submit a form login request', () => {
+    it('go to profile page', () => {
+      cy.intercept(
+        'https://instalura-api-git-master-omariosouto.vercel.app/api/login'
+      ).as('userLogin');
 
-    cy.visit('/app/login/');
+      const loginScreen = new LoginScreenPageObject(cy);
 
-    cy.get('#formCadastro input[name="usuario"]').type('omariosouto');
-    cy.get('#formCadastro input[name="senha"]').type('senhasegura');
+      loginScreen
+        .fillLoginForm({ user: 'omariosouto', password: 'senhasegura' })
+        .submitLoginForm();
 
-    cy.get('#formCadastro button[type="submit"]').click();
+      cy.url().should('include', '/app/profile');
 
-    cy.url().should('include', '/app/profile');
-
-    cy.wait('@userLogin').then((intercept) => {
-      const { token } = intercept.response.body.data;
-      // Verifica se o token existe
-      cy.getCookie('APP_TOKEN')
-        .should('exist')
-        .should('have.property', 'value', token);
+      cy.wait('@userLogin').then((intercept) => {
+        const { token } = intercept.response.body.data;
+        cy.getCookie('APP_TOKEN')
+          .should('exist')
+          .should('have.property', 'value', token);
+      });
     });
   });
 });
